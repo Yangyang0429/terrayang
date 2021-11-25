a1fac580e64d099649800a1945440b347146e7c7
Yangyang0429 <88362207+Yangyang0429@users.noreply.github.com>
2021-11-25 14:17:42 +0900

    Delete 02_ssh.tf

--- 02_ssh.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_key_pair" "yang123_key" {
-    key_name = "yang123-key"
-    public_key = file("../../.ssh/yang123-key.pub")
-    #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6h0i8KhJL5mawFzISFUkoIFXOlx1j6eRQcy1WP73MReKlg+hCb4891cDDp4HF/mPpi0AVAMVFNg0VtjMRBXTBvS8pyWXOnzjMZEsMndSdmeS+dRZtDSvpoE8PcqwNVRSWvhm31rWz7YA8gVEcNGE9ET/1dYR3GYDh3ah5GWdAnhBOD2GNfVZ07R89inEeu6HAOLMIKXBHHXX+hYy7fX5wX74trSaejF+kHF1J0XBAb37OilCrVue7Kqc4V5MnQrCI338V+s8hkQz9ccPJR4+ewcnpD62Hr8G0sCFuhKmTg0rDSdfEtO70aDAKxxYqtv8NKnbGKHlcFgW5co4jb65Wdh4l8IhuS943G2W6CDCa91ZkmzNq3EunqfSD03NhxUEKQ8OgQhPpI1wC4v71H6j6+0HpJtvF0eNMMJ4+P9l7fqPQjnc15YAM9FsbZw9+BV7nzlMHzMsQD55IO4iUi1gK2aoJF6V+dQFfWz9Tf673U5M92DYgxE5uqZYZFifbOks="
-}
